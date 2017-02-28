ba9405551f7277df73332e5897d76d0e2b0ff88c
Priscilla Nguyen <beeyen.lai@gmail.com>
2017-02-28 10:57:23 -0500

    Complete reveal on scroll

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,7 +1,10 @@
 // include jquery in js library
 // var $ = require('jquery');
 // var Person = require('./modules/Person');
-import $ from 'jquery'; // ES6
-
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+
 var mobileMenu = new MobileMenu();
+var featureReveal = new RevealOnScroll($(".feature-item"), "85%");
+var testimonialReveal = new RevealOnScroll($(".testimonial"), "60%");
